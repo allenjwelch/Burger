0e6ac666f3527dd47e286587241f7d6115938179
Allen Welch <allenjwelch@gmail.com>
2018-05-17 17:48:49 -0400

    added framework for main.handlebars

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parcer");
+let exphbs = require("express-handlebars");
+let routes = require("/controllers/burgers_controller.js");
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
